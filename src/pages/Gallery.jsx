b272958b0e038ffce177c012fdeb724f1724f00d
Maksim Dudarev <maxim.d132213@gmail.com>
2023-11-24 17:20:45 +0300

    add slider mobile

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -1,10 +1,10 @@
 import './../styles/gallery.css';
 import { useState, useEffect } from 'react';
+import testimg from './../images/about-img.png';
 
 const Gallery = () => {
 
     const [gallery, setGallery] = useState(null);
-
 
     useEffect(() => {
         const fetchGallery = async () => {
@@ -26,21 +26,49 @@ const Gallery = () => {
 
                 {gallery ?
 
-                    <div className="row" style={{ marginTop: "30px" }}>
-                        {gallery.map((el) => (
-                            <div key={el.id} className="col-md-4 py-3 py-md-0 mb-4">
-                                <div key = {el.id} className="card">
-                                    <img src={el.path} alt="p1" height={'230px'} />
+                    <>
+                        <div className="row full" style={{ marginTop: "30px" }}>
+                            {gallery.map((el) => (
+                                <div key={el.id} className="col-md-4 py-3 py-md-0 mb-4">
+                                    <div key={el.id} className="card">
+                                        <img src={el.path} alt="p" height={'230px'} />
+                                    </div>
                                 </div>
+                            ))}
+                        </div>
+
+                        <div className="row min" style={{ marginTop: "30px" }}>
+                            <div id="carouselControls" className="carousel slide" data-bs-ride="carousel">
+                                <div className="carousel-inner">
+
+                                    {/* <div className="carousel-item active">
+                                <img src={testimg} className="d-block w-100" alt="..." />
+                            </div> */}
+
+                                    {gallery.map((el) => (
+                                        <div className="carousel-item active" key={el.id}>
+                                            <img src={el.path} className="d-block w-100" alt="p" />
+                                        </div>
+                                    ))}
+                                </div>
+                                <button className="carousel-control-prev" type="button" data-bs-target="#carouselControls" data-bs-slide="prev">
+                                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                                    <span className="visually-hidden">Previous</span>
+                                </button>
+                                <button className="carousel-control-next" type="button" data-bs-target="#carouselControls" data-bs-slide="next">
+                                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                                    <span className="visually-hidden">Next</span>
+                                </button>
                             </div>
-                        ))}
-                    </div>
+                        </div>
+                    </>
 
                     : null}
 
+
+
+
             </div>
-
-
 
         </section>
     )
